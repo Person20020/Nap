d6d14b78eee1a08ec37f7cbdee21736177f974d1
Koji Ino <kojiino@outlook.com>
2026-04-28 20:57:35 -1000

    Created website

---
+++ site/README.md
@@ -0,0 +1,8 @@
+# Nap site
+
+This is the website for the app. It a simple Flask app run with docker compose.
+
+
+```bash
+docker compose up -d
+```
---
+++ site/app.py
@@ -0,0 +1,41 @@
+import logging
+import os
+
+import flask
+
+DEBUG = os.getenv("DEBUG", "false") == "true"
+
+PLAUSIBLE_SRC_URL = os.getenv("PLAUSIBLE_SRC_URL", "")
+PLAUSIBLE_DATA_API = os.getenv("PLAUSIBLE_DATA_API", "")
+PLAUSIBLE_DATA_DOMAIN = os.getenv("PLAUSIBLE_DATA_DOMAIN", "")
+
+logging.basicConfig(
+    level=logging.DEBUG if DEBUG else logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
+
+app = flask.Flask(__name__)
+
+
+@app.context_processor
+def inject_plausible():
+    return {
+        "PLAUSIBLE_SRC_URL": PLAUSIBLE_SRC_URL,
+        "PLAUSIBLE_DATA_API": PLAUSIBLE_DATA_API,
+        "PLAUSIBLE_DATA_DOMAIN": PLAUSIBLE_DATA_DOMAIN,
+    }
+
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return flask.render_template("404.html"), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=DEBUG, port=5000)
